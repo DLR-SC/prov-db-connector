7f8d90136c78d6bae05f40ad4534cedae9eaf772
Dr4k3 <Dr4k3@fastreboot.de>
2016-09-20 23:03:07 +0200

    Changed travis and docker-compose setup

--- tests/databases/neo4j/test_neo4jconnector.py
+++ tests/databases/neo4j/test_neo4jconnector.py
@@ -1,10 +1,14 @@
 import unittest
-
+from provdbconnector.databases import Neo4jConnector
 
 class Neo4jConnectorTests(unittest.TestCase):
 
     def setUp(self):
         pass
+
+    def test_connect(self):#
+        neo4j = Neo4jConnector()
+        neo4j.connect()
 
     def tearDown(self):
         pass
--- tests/test_provapi.py
+++ tests/test_provapi.py
@@ -1,9 +1,14 @@
 import unittest
-
+from provdbconnector import ProvApi
 
 class ProvApiTests(unittest.TestCase):
 
+
     def setUp(self):
+        pass
+
+    def test_create_document(self):
+        provneo4j = ProvApi()
         pass
 
     def tearDown(self):
