b71a893981f42c66fd39d18e685a374405f8a9ae
Dr4k3 <Dr4k3@fastreboot.de>
2016-09-20 23:25:48 +0200

    Added test for connection to docker container

--- tests/databases/neo4j/test_neo4jconnector.py
+++ tests/databases/neo4j/test_neo4jconnector.py
@@ -6,10 +6,6 @@ class Neo4jConnectorTests(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_connect(self):#
-        neo4j = Neo4jConnector()
-        neo4j.connect()
-
     def tearDown(self):
         pass
 
--- tests/test_provapi.py
+++ tests/test_provapi.py
@@ -3,12 +3,7 @@ from provdbconnector import ProvApi
 
 class ProvApiTests(unittest.TestCase):
 
-
     def setUp(self):
-        pass
-
-    def test_create_document(self):
-        provneo4j = ProvApi()
         pass
 
     def tearDown(self):
