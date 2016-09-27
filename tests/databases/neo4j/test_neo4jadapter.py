f5664368f5d027dffc8a1a8bc3ab43f638c959b6
Stefan B <Stefan.bie@live.de>
2016-09-27 13:40:34 +0200

    - fix test

--- tests/databases/neo4j/test_neo4jadapter.py
+++ tests/databases/neo4j/test_neo4jadapter.py
@@ -5,29 +5,33 @@ from provdbconnector.databases import InvalidOptionsException, AuthException
 from tests.databases.test_baseadapter import AdapterTestTemplate
 
 
+neo4j_default_user = os.environ.get('NEO4J_USERNAME', 'neo4j')
+neo4j_default_pass = os.environ.get('NEO4J_PASSWORD', 'neo4jneo4j')
+neo4j_default_host = os.environ.get('NEO4J_HOST', 'localhost:7687')
+
 class Neo4jAdapterTests(AdapterTestTemplate):
 
     def setUp(self):
         self.instance = Neo4jAdapter()
-        authInfo = {"user_name": os.environ.get('NEO4J_USERNAME', 'neo4j'),
-                    "user_password": os.environ.get('NEO4J_PASSWORD', 'neo4jneo4j'),
-                    "host": os.environ.get('NEO4J_HOST', 'localhost:7687')
+        authInfo = {"user_name": neo4j_default_user,
+                    "user_password": neo4j_default_pass,
+                    "host": neo4j_default_host
         }
         self.instance.connect(authInfo)
 
     def test_connect_fails(self):
-        authInfo = {"user_name": 'neo4j',
+        authInfo = {"user_name": neo4j_default_user,
                     "user_password": 'xxxxxx',
-                    "host": 'localhost:7687'
+                    "host": neo4j_default_host
         }
         self.instance.connect(authInfo)
         with self.assertRaises(AuthException):
             self.instance.connect(authInfo)
 
     def test_connect_invalid_options(self):
-        authInfo = {"u": 'neo4j',
+        authInfo = {"u": neo4j_default_user,
                     "p": 'xxxxxx',
-                    "h": 'localhost:7687'
+                    "h": neo4j_default_host
         }
         with self.assertRaises(InvalidOptionsException):
             self.instance.connect(authInfo)
