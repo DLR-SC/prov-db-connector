fc1b54b3b10371ef6d2fa4347274e4280638e414
Stefan B <Stefan.bie@live.de>
2016-09-27 10:23:27 +0200

    - add int and float as primitive

--- provdbconnector/utils/serializer.py
+++ provdbconnector/utils/serializer.py
@@ -2,12 +2,18 @@ from datetime import datetime
 
 from prov.model import QualifiedName,Identifier,Literal
 import sys
-def encode_string_value_to_premitive(value):
+def encode_string_value_to_primitive(value):
     if sys.version_info[0] < 3:
         if type(value) is unicode:
             return value.encode("utf8")
     if isinstance(value, Literal):
         return value.value
+    elif type(value) is int:
+        return value
+    elif type(value) is float:
+        return value
     elif type(value) is bool:
         return value
     return str(value)
+
+
