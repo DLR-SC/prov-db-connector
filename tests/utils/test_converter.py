a42027d28d5b9ac269ea9cb4e3155bded8def8ef
Dr4k3 <Dr4k3@fastreboot.de>
2016-09-27 10:44:11 +0200

    Add type check tests

--- tests/utils/test_converter.py
+++ tests/utils/test_converter.py
@@ -1,9 +1,11 @@
 import unittest
-from prov.model import ProvRecord, ProvDocument
+from xml.etree import ElementTree
+import pkg_resources
+import json
+
+from prov.model import ProvDocument
 from prov.tests import examples
 from provdbconnector.utils import to_json, from_json, to_provn, from_provn, to_xml, from_xml
-import pkg_resources
-
 from provdbconnector.utils import NoDocumentException
 
 
@@ -17,7 +19,6 @@ class ConverterTests(unittest.TestCase):
         #   <your_package>/templates/temp_file
         #   the correct way to read your template is to use pkg_resources package from setuptools distribution:
         test_resources = {
-            #'xml':{'package':'prov','file':'tests/xml/prov_primer_complete_example.xml'},
             'xml': {'package': 'provdbconnector', 'file': '../tests/resources/primer.provx'},
             'json':{'package':'provdbconnector', 'file':'../tests/resources/primer.json'},
             'provn':{'package':'provdbconnector', 'file':'../tests/resources/primer.provn'}
@@ -31,29 +32,35 @@ class ConverterTests(unittest.TestCase):
     def test_to_json(self):
         self.assertRaises(NoDocumentException, lambda: to_json())
         json_document = to_json(self.prov_document)
-        #print(self.test_files['json'].read().decode('utf-8'))
-        # TODO Add check if equal or rely on prov package tests?
+        self.assertIsInstance(json_document, str)
+        try:
+            json.loads(json_document)
+        except ValueError:
+            self.fail("Invalid JSON")
 
     def test_from_json(self):
         self.assertRaises(NoDocumentException, lambda: from_json())
-        from_json(self.test_files['json'])
-        # TODO Add check if equal or rely on prov package tests?
+        prov = from_json(self.test_files['json'])
+        self.assertIsInstance(prov, ProvDocument)
 
     def test_to_provn(self):
         self.assertRaises(NoDocumentException, lambda: to_provn())
         provn_document = to_provn(self.prov_document)
-        # TODO Add check if equal or rely on prov package tests?
+        self.assertIsInstance(provn_document, str)
+        #Validate that string is in provn format
 
     def test_from_provn(self):
         self.assertRaises(NoDocumentException, lambda: from_provn())
         prov = from_provn(self.test_files['provn'])
+        self.assertIsInstance(prov, ProvDocument)
 
     def test_to_xml(self):
         self.assertRaises(NoDocumentException, lambda: to_xml())
         xml_document = to_xml(self.prov_document)
-        # TODO Add check if equal or rely on prov package tests?
+        self.assertIsInstance(xml_document, str)
+        ElementTree.fromstring(xml_document)
 
     def test_from_xml(self):
         self.assertRaises(NoDocumentException, lambda: from_xml())
         prov = from_xml(self.test_files['xml'])
-        # TODO Add check if equal or rely on prov package tests?
+        self.assertIsInstance(prov, ProvDocument)
