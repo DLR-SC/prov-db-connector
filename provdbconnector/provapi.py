4354d80b45a70264880c8d302f4e6542d9bd23f4
Stefan B <Stefan.bie@live.de>
2016-09-28 12:55:41 +0200

    - implemented the function get_metadata_and_attributes - add some serializer code

--- provdbconnector/provapi.py
+++ provdbconnector/provapi.py
@@ -1,6 +1,9 @@
 from uuid import uuid4
-from prov.model import  ProvDocument, ProvBundle
-
+from prov.model import  ProvDocument, ProvBundle, ProvRecord,ProvRelation, QualifiedName
+from prov.constants import PROV_ATTRIBUTES
+from provdbconnector.databases.baseadapter import METADATA_KEY_PROV_TYPE,METADATA_PARENT_ID,METADATA_KEY_LABEL,METADATA_KEY_NAMESPACES,METADATA_KEY_BUNDLE_ID,METADATA_KEY_TYPE_MAP
+from provdbconnector.utils.serializer import encode_json_representation
+from collections import namedtuple
 class ProvApiException(Exception):
     pass
 
@@ -10,7 +13,8 @@ class NoDataBaseAdapterException(ProvApiException):
 
 class InvalidArgumentTypeException(ProvApiException):
     pass
-
+class InvalidProvRecordException(ProvApiException):
+    pass
 class ProvApi(object):
     def __init__(self, id=None, adapter=None, authinfo=None, *args):
         if id is None:
@@ -49,8 +53,9 @@ class ProvApi(object):
 
         prov_document = content
 
-        # create document id
-        # create_bundle(document_id, prov_document)
+        doc_id = self._adapter.create_document()
+
+        self._create_bundle(doc_id,prov_document,bundle_connection=False)
 
         # foreach bundle
         #    create bundle id
@@ -87,3 +92,67 @@ class ProvApi(object):
         #        if the relation type is"prov:mentionOf" (https://www.w3.org/TR/prov-links/)
         #             create relation
         pass
+
+    def _get_metadata_and_attributes_for_record(self, prov_record):
+        if not isinstance(prov_record, ProvRecord):
+            raise InvalidArgumentTypeException()
+
+        used_namespaces = dict()
+        bundle = prov_record.bundle
+
+        prov_type = prov_record.get_type()
+        prov_label = prov_record.label
+
+
+        # Be sure that the prov_label is a qualified name instnace
+
+        if not isinstance(prov_label, QualifiedName):
+            qualified_name = bundle.valid_qualified_name(prov_label)
+            if qualified_name is None:
+                raise InvalidProvRecordException(
+                    "The prov record {} is invalid because the prov_label {} can't be qualified".format(prov_record,
+                                                                                                        prov_label))
+            else:
+                prov_label =  qualified_name
+
+        #extract namespaces from record
+
+        attributes = dict(prov_record.attributes.copy())
+        for key,value in attributes.items():
+
+            #ensure key is QualifiedName
+            if isinstance(key, QualifiedName):
+                namespace = key.namespace
+                used_namespaces.update({str(namespace.prefix): str(namespace.uri)})
+            else:
+                raise InvalidProvRecordException("Not support key type %s" % type(key))
+
+            #try to add
+            if isinstance(value, QualifiedName):
+                namespace = value.namespace
+                used_namespaces.update({str(namespace.prefix): str(namespace.uri)})
+            else:
+                qualified_name = bundle.valid_qualified_name(value)
+                if qualified_name is not None:
+                    attributes[key] = qualified_name # update attribute
+                    namespace = qualified_name.namespace
+                    used_namespaces.update({str(namespace.prefix): str(namespace.uri)})
+
+        #create type dict
+        types_dict = dict()
+        for key,value in attributes.items():
+            if key not in PROV_ATTRIBUTES:
+                type = encode_json_representation(value)
+                if type is not None:
+                    types_dict.update({str(key): type})
+
+        metadata = {
+            METADATA_KEY_PROV_TYPE: prov_type,
+            METADATA_KEY_LABEL: prov_label,
+            METADATA_KEY_NAMESPACES: used_namespaces,
+            METADATA_KEY_TYPE_MAP: types_dict
+        }
+        MetaAndAttributes = namedtuple("MetaAndAttributes", "metadata, attributes")
+
+        return MetaAndAttributes(metadata,attributes)
+
--- provdbconnector/utils/serializer.py
+++ provdbconnector/utils/serializer.py
@@ -1,5 +1,23 @@
-from prov.model import Literal
+from prov.model import Literal,Identifier, QualifiedName
+from prov.constants import PROV_QUALIFIEDNAME
+from datetime import datetime
+import six
 import sys
+
+
+# Reverse map for prov.model.XSD_DATATYPE_PARSERS
+LITERAL_XSDTYPE_MAP = {
+    float: 'xsd:double',
+    int: 'xsd:int'
+    # boolean, string values are supported natively by PROV-JSON
+    # datetime values are converted separately
+}
+
+# Add long on Python 2
+if six.integer_types[-1] not in LITERAL_XSDTYPE_MAP:
+    LITERAL_XSDTYPE_MAP[six.integer_types[-1]] = 'xsd:long'
+
+
 
 def encode_dict_values_to_primitive(dict_values):
     dict_values = dict_values.copy()
@@ -7,6 +25,7 @@ def encode_dict_values_to_primitive(dict_values):
         dict_values[key] = encode_string_value_to_primitive(value)
 
     return dict_values
+
 def encode_string_value_to_primitive(value):
     if sys.version_info[0] < 3:
         if type(value) is unicode:
@@ -23,4 +42,28 @@ def encode_string_value_to_primitive(value):
         return value
     return str(value)
 
+
+def literal_json_representation(literal):
+    # TODO: QName export
+    value, datatype, langtag = literal.value, literal.datatype, literal.langtag
+    if langtag:
+        return {'lang': langtag}
+    else:
+        return {'type': six.text_type(datatype)}
+
+def encode_json_representation(value):
+    if isinstance(value, Literal):
+        return literal_json_representation(value)
+    elif isinstance(value, datetime):
+        return {'type': 'xsd:dateTime'}
+    elif isinstance(value, QualifiedName):
+        # TODO Manage prefix in the whole structure consistently
+        # TODO QName export
+        return {'type': PROV_QUALIFIEDNAME._str}
+    elif isinstance(value, Identifier):
+        return {'type': 'xsd:anyURI'}
+    elif type(value) in LITERAL_XSDTYPE_MAP:
+        return {'type': LITERAL_XSDTYPE_MAP[type(value)]}
+    else:
+        return None
 
