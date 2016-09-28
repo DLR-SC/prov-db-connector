ed00582debc782a06428e2c9600b53556bc119d4
Stefan B <Stefan.bie@live.de>
2016-09-28 10:54:48 +0200

    - add pseudo code

--- provdbconnector/provapi.py
+++ provdbconnector/provapi.py
@@ -1,5 +1,5 @@
 from uuid import uuid4
-from prov.model import  ProvDocument
+from prov.model import  ProvDocument, ProvBundle
 
 class ProvApiException(Exception):
     pass
@@ -47,5 +47,43 @@ class ProvApi(object):
         if not isinstance(content,ProvDocument):
             raise InvalidArgumentTypeException()
 
+        prov_document = content
+
+        # create document id
+        # create_bundle(document_id, prov_document)
+
+        # foreach bundle
+        #    create bundle id
+        #    self._create_bundle(bundle_id, bundle)
+
+
+        # foreach bundle in bundles
+        #    self._create_bundle_links(bundle_id, prov_bundle)
+
+
     def get_document_as_prov(self, id=None):
         raise NotImplementedError()
+
+
+    def _create_bundle(self,bundle_id,prov_bundle, bundle_connection=True):
+        if not isinstance(prov_bundle, ProvBundle) or type(bundle_id) is not str:
+            raise InvalidArgumentTypeException()
+
+        #    foreach record in bundle :
+        #        prepare metadata (like labels, namespaces, type_map) as primitive datatypes
+        #        create database node
+        #        create relation between bundle node and record node, only if options (bundle_connection) is set
+
+
+        #    foreach relation in bundle:
+        #        skip relations of the type "prov:mentionOf"
+        #        if target or origin record is unknown, create node "Unknown"
+        #        prepare metadata (like labels, namespaces, type_map) as primitive datatypes
+        #        create database relation
+
+    def _create_bundle_links(self,prov_bundle):
+
+        #   foreach relation in bundle
+        #        if the relation type is"prov:mentionOf" (https://www.w3.org/TR/prov-links/)
+        #             create relation
+        pass
