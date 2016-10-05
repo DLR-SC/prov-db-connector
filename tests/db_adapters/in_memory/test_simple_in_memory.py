67baa2501b3afced3029e62a5cbeaa38c3649ed6
Stefan B <Stefan.bie@live.de>
2016-10-05 11:29:50 +0200

    - set up test for InMemoryAdapter

--- tests/db_adapters/in_memory/test_simple_in_memory.py
+++ tests/db_adapters/in_memory/test_simple_in_memory.py
@@ -6,25 +6,25 @@ from tests.db_adapters.test_baseadapter import AdapterTestTemplate
 from tests.test_provapi import ProvApiTestTemplate
 
 
-# class SimpleInMemoryAdapterTest(AdapterTestTemplate):
-#
-#     def setUp(self):
-#         self.instance = SimpleInMemoryAdapter()
-#         self.instance.connect(None)
-#
-#     def test_connect_invalid_options(self):
-#         authInfo = {"invalid": "Invalid"}
-#         with self.assertRaises(InvalidOptionsException):
-#             self.instance.connect(authInfo)
-#
-#     def tearDown(self):
-#         del self.instance
+class SimpleInMemoryAdapterTest(AdapterTestTemplate):
+
+    def setUp(self):
+        self.instance = SimpleInMemoryAdapter()
+        self.instance.connect(None)
+
+    def test_connect_invalid_options(self):
+        authInfo = {"invalid": "Invalid"}
+        with self.assertRaises(InvalidOptionsException):
+            self.instance.connect(authInfo)
+
+    def tearDown(self):
+        del self.instance
 
 
-# class Neo4jAdapterProvApiTests(ProvApiTestTemplate):
-#
-#     def setUp(self):
-#         self.provapi = ProvApi(id=1, adapter=Neo4jAdapter, authinfo=None)
-#
-#     def tearDown(self):
-#         del self.provapi
+class SimpleInMemoryAdapterProvApiTests(ProvApiTestTemplate):
+
+     def setUp(self):
+         self.provapi = ProvApi(id=1, adapter=SimpleInMemoryAdapter, authinfo=None)
+
+     def tearDown(self):
+         del self.provapi
