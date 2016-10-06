b818a465deab2c792fca449b5887a4d9267c208a
Stefan B <Stefan.bie@live.de>
2016-10-06 12:02:26 +0200

    removed wildcard import

--- provdbconnector/__init__.py
+++ provdbconnector/__init__.py
@@ -1,4 +1,3 @@
 from provdbconnector.provapi import ProvApi
 from provdbconnector.db_adapters import BaseAdapter
 from provdbconnector.db_adapters.neo4j import Neo4jAdapter, NEO4J_USER, NEO4J_PASS, NEO4J_HOST, NEO4J_HTTP_PORT, NEO4J_BOLT_PORT
-from provdbconnector.tests import  *
