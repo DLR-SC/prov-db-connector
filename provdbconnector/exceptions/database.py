7c33bda5ea9de87cc4e73c9e640c37c6231aef81
Martin Stoffers <Dr4k3@fastreboot.de>
2016-10-25 18:13:33 +0200

    Add docu to exceptions

--- provdbconnector/exceptions/database.py
+++ provdbconnector/exceptions/database.py
@@ -2,28 +2,49 @@ from .provapi import ProvApiException
 
 
 class AdapterException(ProvApiException):
+    """
+    Base exception class for database adapters.
+    """
     pass
 
 
 class InvalidOptionsException(AdapterException):
+    """
+    Thrown, if passed argument for adapter is invalid.
+    """
     pass
 
 
 class AuthException(AdapterException):
+    """
+    Thrown, if database adapter could not establish a connection with given credentials to the database.
+    """
     pass
 
 
 class DatabaseException(AdapterException):
+    """
+    Thrown, if method could not performed on database.
+    """
     pass
 
 
 class CreateRecordException(DatabaseException):
+    """
+    Thrown, if record could not be saved in database.
+    """
     pass
 
 
 class CreateRelationException(DatabaseException):
+    """
+    Thrown, if relation could not be saved in database.
+    """
     pass
 
 
 class NotFoundException(DatabaseException):
+    """
+    Thrown, if record or relation could not be found in database.
+    """
     pass
