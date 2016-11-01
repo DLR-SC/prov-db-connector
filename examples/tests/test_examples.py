8338b3acd5df582d13b4fe20e3a4bcbaacddf95c
Stefan B <Stefan.bie@live.de>
2016-11-01 14:18:50 +0100

    - add tests for example scripts (to be sure that the example is helpful;))

---
+++ examples/tests/test_examples.py
@@ -0,0 +1,22 @@
+import unittest
+
+
+class ExamplesTest(unittest.TestCase):
+
+    def test_bundle_example(self):
+        import examples.bundle_example
+
+
+    def test_test_complex_example_with_neo4j(self):
+        import examples.complex_example_with_neo4j
+
+
+    def test_file_buffer_example(self):
+        import examples.file_buffer_example
+
+
+    def test_simple_example(self):
+        import examples.simple_example
+
+    def simple_example_with_neo4j(self):
+        import examples.simple_example_with_neo4j
--- provdbconnector/tests/__init__.py
+++ provdbconnector/tests/__init__.py
@@ -1,2 +1,6 @@
 from provdbconnector.tests.db_adapters.test_baseadapter import AdapterTestTemplate
 from provdbconnector.tests.test_provapi import ProvApiTestTemplate
+import unittest
+def additional_tests():
+    from examples.tests.test_examples import ExamplesTest
+    return unittest.defaultTestLoader.loadTestsFromTestCase(ExamplesTest)
