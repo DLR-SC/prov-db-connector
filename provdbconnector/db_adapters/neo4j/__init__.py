67c78b57ea691e9143162966f999aeb331793e80
Stefan B <Stefan.bie@live.de>
2016-11-07 11:38:05 +0100

    changed cypher_commands import (because readthedocs.io.)

--- provdbconnector/db_adapters/neo4j/__init__.py
+++ provdbconnector/db_adapters/neo4j/__init__.py
@@ -1,3 +1,4 @@
 from provdbconnector.db_adapters.neo4j.neo4jadapter import Neo4jAdapter
+import provdbconnector.db_adapters.neo4j.cypher_commands as cypher_commands
 from provdbconnector.db_adapters.neo4j.neo4jadapter import NEO4J_USER, NEO4J_PASS
 from provdbconnector.db_adapters.neo4j.neo4jadapter import NEO4J_HOST, NEO4J_BOLT_PORT, NEO4J_HTTP_PORT
