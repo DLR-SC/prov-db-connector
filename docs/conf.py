8028ad9e5cbe8e5fc0eb1ebe4092ff5c9c1fddf5
Stefan B <Stefan.bie@live.de>
2016-11-07 10:43:31 +0100

    - try to fix readthedocs.io

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@
 #
 import os
 import sys
-#sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('..'))
 #sys.path.insert(1, os.path.abspath('./../../provdbconnector'))
 #sys.path.insert(2, os.path.abspath('./../../'))
 
