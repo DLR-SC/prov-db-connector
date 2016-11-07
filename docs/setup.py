f5787277f6f6e8c970ad0ed4436664fae3172f93
Stefan B <Stefan.bie@live.de>
2016-11-07 10:26:37 +0100

    - try something to fix docs

---
+++ docs/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+tests_require = [
+    'coverage',
+    'coveralls'
+]
+
+docs_require = [
+    'Sphinx>=1.3.5',
+    'recommonmark>=0.4.0',
+    'sphinx-rtd-theme>=0.1.9',
+    'sphinxcontrib-napoleon>=0.4.4',
+    'sphinxcontrib-httpdomain>=1.5.0',
+]
+
+setup(
+    name='prov-db-connector',
+    version='0.2',
+    description='PROV Database Connector',
+    keywords=[
+        'provenance', 'graph', 'model', 'PROV', 'PROV-DM', 'PROV-JSON', 'JSON',
+        'PROV-XML', 'PROV-N'
+    ],
+    author='DLR, Stefan Bieliauskas, Martin Stoffers',
+    author_email='opensource@dlr.de, sb@conts.de, martin.stoffers@studserv.uni-leipzig.de',
+    url='https://github.com/DLR-SC/prov-db-connector',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: Apache License 2.0',
+        'Programming Language :: Python 3.4',
+    ],
+    license="Apache License 2.0",
+
+    packages=find_packages(),
+    package_dir={
+        'provdbconnector': 'provdbconnector'
+    },
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "prov==1.5.0",
+        "neo4j-driver==1.0.2",
+        "networkx==1.11",
+        "decorator==4.0.10",
+        "lxml==3.6.4",
+        "six==1.10.0"
+    ],
+    extras_require={
+        'test': tests_require,
+        'dev': tests_require + docs_require,
+        'docs': docs_require,
+    },
+
+    test_suite='provdbconnector.tests',
+)
