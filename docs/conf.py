56b6f264f28f24c3982b35e6268a8d930ca5f82f
Stefan B <Stefan.bie@live.de>
2016-11-07 12:08:40 +0100

    - try to fix readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -35,18 +35,17 @@ project_root = os.path.dirname(cwd)
 # version is used.
 sys.path.insert(0, project_root)
 
-import prov
 
-#
-# from mock import Mock as MagicMock
-#
-# class Mock(MagicMock):
-#     @classmethod
-#     def __getattr__(cls, name):
-#             return Mock()
-#
-# MOCK_MODULES = ['prov','prov.constants', 'prov.model', 'abc', "neo4j.v1.exceptions", "neo4j.v1"]
-# sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+ @classmethod
+ def __getattr__(cls, name):
+         return Mock()
+
+MOCK_MODULES = ['prov','prov.constants', 'prov.model']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 from recommonmark.parser import CommonMarkParser
 import sphinx_rtd_theme
