2917db5817797cef34bb019856bb5d91c522d14b
Stefan B <Stefan.bie@live.de>
2016-11-07 12:25:01 +0100

    - removed imports from init.py and changed root init

--- provdbconnector/__init__.py
+++ provdbconnector/__init__.py
@@ -2,9 +2,7 @@ from provdbconnector.provapi import ProvApi
 
 from provdbconnector.exceptions.provapi import ProvApiException
 
-import provdbconnector.db_adapters  as db_adapters
-
-from provdbconnector.db_adapters.neo4j import Neo4jAdapter
-from provdbconnector.db_adapters.neo4j import NEO4J_USER, NEO4J_PASS, NEO4J_HOST, NEO4J_HTTP_PORT, NEO4J_BOLT_PORT
+from provdbconnector.db_adapters.neo4j.neo4jadapter import Neo4jAdapter
+from provdbconnector.db_adapters.neo4j.neo4jadapter import NEO4J_USER, NEO4J_PASS, NEO4J_HOST, NEO4J_HTTP_PORT, NEO4J_BOLT_PORT
 
 from provdbconnector.db_adapters.in_memory import SimpleInMemoryAdapter
--- provdbconnector/db_adapters/neo4j/__init__.py
+++
@@ -1,4 +0,0 @@
-from provdbconnector.db_adapters.neo4j.neo4jadapter import Neo4jAdapter
-import provdbconnector.db_adapters.neo4j.cypher_commands as cypher_commands
-from provdbconnector.db_adapters.neo4j.neo4jadapter import NEO4J_USER, NEO4J_PASS
-from provdbconnector.db_adapters.neo4j.neo4jadapter import NEO4J_HOST, NEO4J_BOLT_PORT, NEO4J_HTTP_PORT
