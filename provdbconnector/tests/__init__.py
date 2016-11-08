b1cff136dd5fec4177dc61f62c319993fca87738
Stefan B <Stefan.bie@live.de>
2016-11-08 14:21:46 +0100

    import ProvApiTestTemplate

--- provdbconnector/tests/__init__.py
+++ provdbconnector/tests/__init__.py
@@ -1,4 +1,7 @@
+from provdbconnector.tests.db_adapters.test_baseadapter import AdapterTestTemplate
+from provdbconnector.tests.test_provapi import ProvApiTestTemplate
 import unittest
+
 
 def additional_tests():
     from examples.tests.test_examples import ExamplesTest
