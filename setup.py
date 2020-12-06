f5100362ce7c69958002ecf5dff308648d1893a5
Stefan <opensource@conts.de>
2020-12-06 15:03:24 +0100

    Remove python 3.5 support

--- setup.py
+++ setup.py
@@ -33,11 +33,11 @@ setup(
         'Development Status :: 1 - Planning',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3.9'
+        'Programming Language :: Python :: 3.10'
     ],
     license="Apache License 2.0",
 
